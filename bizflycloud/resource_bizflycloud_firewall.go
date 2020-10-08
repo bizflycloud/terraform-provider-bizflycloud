9816de141bc24576361890fbc2fd26c4dac919e5
Sa Pham <great_bn@yahoo.com>
2020-10-08 10:55:47 +0700

    Add support firewall resource (#7)
    * Add support firewall resource
    
    * Fix return not check
    
    Co-authored-by: sapd <sapd@vccloud.vn>

---
+++ bizflycloud/resource_bizflycloud_firewall.go
@@ -0,0 +1,196 @@
+// This file is part of gobizfly
+//
+// Copyright (C) 2020  BizFly Cloud
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>
+
+package bizflycloud
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/bizflycloud/gobizfly"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceBizFlyCloudFirewall() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceBizFlyCloudFirewallCreate,
+		Read:   resourceBizFlyCloudFirewallRead,
+		Update: resourceBizFlyCloudFirewallUpdate,
+		Delete: resourceBizFlyCloudFirewallDelete,
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"servers_count": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"rules_count": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"ingress": {
+				Type:       schema.TypeSet,
+				Optional:   true,
+				Computed:   true,
+				ConfigMode: schema.SchemaConfigModeAttr,
+				Elem: &schema.Resource{
+					Schema: firewallRuleSchema(),
+				},
+			},
+			"egress": {
+				Type:       schema.TypeSet,
+				Optional:   true,
+				Computed:   true,
+				ConfigMode: schema.SchemaConfigModeAttr,
+				Elem: &schema.Resource{
+					Schema: firewallRuleSchema(),
+				},
+			},
+			"target_server_ids": {
+				Type:     schema.TypeSet,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func firewallRuleSchema() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		"cidr": {
+			Type:     schema.TypeString,
+			Required: true,
+		},
+		"protocol": {
+			Type:     schema.TypeString,
+			Required: true,
+		},
+		"port_range": {
+			Type:     schema.TypeString,
+			Required: true,
+		},
+	}
+}
+
+func firewallRequestBuilder(d *schema.ResourceData) gobizfly.FirewallRequestPayload {
+	firewallOpts := gobizfly.FirewallRequestPayload{}
+	if v, ok := d.GetOk("name"); ok {
+		firewallOpts.Name = v.(string)
+	}
+	if v, ok := d.GetOk("target_server_ids"); ok {
+		for _, id := range v.(*schema.Set).List() {
+			firewallOpts.Targets = append(firewallOpts.Targets, id.(string))
+		}
+	}
+	if v, ok := d.GetOk("ingress"); ok {
+		firewallOpts.InBound = flatternFirewallRules(v.(*schema.Set))
+	}
+	if v, ok := d.GetOk("egress"); ok {
+		firewallOpts.OutBound = flatternFirewallRules(v.(*schema.Set))
+	}
+	return firewallOpts
+}
+func resourceBizFlyCloudFirewallCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*CombinedConfig).gobizflyClient()
+
+	firewallOpts := firewallRequestBuilder(d)
+	firewall, err := client.Firewall.Create(context.Background(), &firewallOpts)
+	if err != nil {
+		return fmt.Errorf("Error when creating firewall: %v", err)
+	}
+	d.SetId(firewall.BaseFirewall.ID)
+	return resourceBizFlyCloudFirewallRead(d, meta)
+}
+
+func resourceBizFlyCloudFirewallRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*CombinedConfig).gobizflyClient()
+	firewall, err := client.Firewall.Get(context.Background(), d.Id())
+	if err != nil {
+		return fmt.Errorf("Error when retrieving fireall: %v", err)
+	}
+	_ = d.Set("name", firewall.BaseFirewall.Name)
+	_ = d.Set("servers_count", firewall.BaseFirewall.ServersCount)
+	_ = d.Set("rules_count", firewall.BaseFirewall.RulesCount)
+
+	_ = d.Set("target_servers", flatternBizFlyCloudServers(firewall.Servers))
+	if len(firewall.InBound) > 0 {
+		_ = d.Set("ingress", convertFWRule(firewall.InBound))
+	}
+	if len(firewall.OutBound) > 0 {
+		_ = d.Set("egress", convertFWRule(firewall.OutBound))
+	}
+	return nil
+}
+
+func resourceBizFlyCloudFirewallDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*CombinedConfig).gobizflyClient()
+	_, err := client.Firewall.Delete(context.Background(), d.Id())
+	if err != nil {
+		return fmt.Errorf("Error when deleting firewall: %v", err)
+	}
+	return nil
+}
+
+func resourceBizFlyCloudFirewallUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*CombinedConfig).gobizflyClient()
+
+	firewallOpts := firewallRequestBuilder(d)
+	firewall, err := client.Firewall.Update(context.Background(), d.Id(), &firewallOpts)
+	if err != nil {
+		return fmt.Errorf("Error when creating firewall: %v", err)
+	}
+	d.SetId(firewall.BaseFirewall.ID)
+	return resourceBizFlyCloudFirewallRead(d, meta)
+}
+
+func flatternFirewallRules(rules *schema.Set) []gobizfly.FirewallRuleCreateRequest {
+	fwrules := []gobizfly.FirewallRuleCreateRequest{}
+	for _, rawRule := range rules.List() {
+		r := rawRule.(map[string]interface{})
+		rule := gobizfly.FirewallRuleCreateRequest{
+			Type:      "CUSTOM",
+			CIDR:      r["cidr"].(string),
+			PortRange: r["port_range"].(string),
+			Protocol:  r["protocol"].(string),
+		}
+		fwrules = append(fwrules, rule)
+	}
+	return fwrules
+}
+
+func convertFWRule(rules []gobizfly.FirewallRule) []map[string]interface{} {
+	result := make([]map[string]interface{}, len(rules))
+	for i, v := range rules {
+		result[i] = map[string]interface{}{
+			"cidr":       v.CIDR,
+			"port_range": v.PortRange,
+			"protocol":   v.Protocol,
+		}
+	}
+	return result
+}
+
+func flatternBizFlyCloudServers(servers []*gobizfly.Server) *schema.Set {
+	flattenedServers := schema.NewSet(schema.HashString, []interface{}{})
+	for _, server := range servers {
+		flattenedServers.Add(server.ID)
+	}
+	return flattenedServers
+}
---
+++ website/docs/r/firewall.html.markdown
@@ -0,0 +1,66 @@
+---
+layout: "bizflycloud"
+page_title: "BizFly Cloud: bizflycloud_firewall"
+sidebar_current: "docs-bizflycloud-resource-firewall"
+description: |-
+  Provides a BizFly Cloud Firewall resource. This can be used to create, modify, and delete firewall.
+---
+
+# bizflycloud\_firewall
+
+Provides a BizFly Cloud Firewal resource. This can be used to create,
+modify, and delete firewall.
+
+## Example Usage
+
+```hcl
+# Create a new firewall and attach to a server
+resource "bizflycloud_firewall" "fw1" {
+    name = "sapd-firewall-2"
+    ingress {
+        cidr = "0.0.0.0/0"
+        port_range = "80"
+        protocol = "tcp"
+    }
+    ingress {
+        cidr = "192.168.1.0/24"
+        port_range = "8000"
+        protocol = "udp"
+    }
+    egress {
+        cidr = "0.0.0.0/0"
+        port_range = "80"
+        protocol = "tcp"
+    }
+    target_server_ids = [
+        "${bizflycloud_server.sapd-ubuntu-20.id}"
+    ]
+
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `name` - (Required) Name of the firewall
+* `target_server_ids` - (Optional) - List ID of the server which will be applied the firewall
+* `ingress` - (Optional) Can be specified multiple times for each ingress rule. Each ingress block supports fields documented below
+* `egress` - (Optional) Can be specified multiple times for each egress rule. Each egress block supports fields documented below.
+
+The `ingress` and `egress` block supprts:
+
+* `cidr` - (Required) CIDR Block: IPv4 or IPv6 CIDR
+* `port_range` - (Required) Port range. Example: `80` or `8000-9000`
+* `protocol` - (Required) Layer 4 protocol.  Available: tcp or udp
+
+## Attributes Reference
+
+The following attributes are exported:
+
+* `id` - The ID of the firewall
+* `name`- The name of the firewall
+* `target_server_ids` - List ID of the server which will be applied the firewall
+* `servers_count` - Number of server are applied the firewall
+* `rules_count` - Number of rules of the firewall
+
