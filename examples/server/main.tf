4b52beceda37dc669c9789b340b14acdc7f02508
Do Son Tung <dosontung007@gmail.com>
2022-08-16 21:20:43 +0700

    feat: support action update to update network plan, billing plan of server (#50)
    Co-authored-by: Tùng Đỗ Sơn <svtt.tungds@vccloud.vn>

--- examples/server/main.tf
+++ examples/server/main.tf
@@ -14,29 +14,36 @@ provider "bizflycloud" {
   password    = ""
 }
 
+resource "bizflycloud_firewall" "sample_firewall_1" {
+  name = "sample-firewall-1"
+}
+
 data "bizflycloud_ssh_key" "ssh_key" {
   name = "test1"
 }
 
+
 data "bizflycloud_volume_type" "example_volume_type" {
-  name = "HDD"
+  name     = "HDD"
   category = "premium"
 }
 
 resource "bizflycloud_server" "tf_server1" {
-  name                   = "tf_server_7"
+  name                   = "tf_server_1"
   flavor_name            = "2c_2g"
   ssh_key                = data.bizflycloud_ssh_key.ssh_key.name
   os_type                = "image"
   os_id                  = "d646476d-850c-423e-b02c-6b86aeda3717"
   category               = "premium"
   availability_zone      = "HN1"
-  root_disk_volume_type         = data.bizflycloud_volume_type.example_volume_type.type
+  root_disk_volume_type  = data.bizflycloud_volume_type.example_volume_type.type
   root_disk_size         = 20
   network_plan           = "free_bandwidth"
-  billing_plan          = "on_demand"
-  wan_network_interfaces = [data.bizflycloud_wan_ip.wan_ip.id, data.bizflycloud_wan_ip.wan_ip_2.id]
-  network_interfaces = [data.bizflycloud_network_interface.lan_ip_1.id, data.bizflycloud_network_interface.lan_ip_2.id]
+  billing_plan           = "on_demand"
+  wan_network_interfaces = [data.bizflycloud_wan_ip.wan_ip.id]
+  network_interfaces     = [
+    data.bizflycloud_network_interface.lan_ip_1.id, data.bizflycloud_network_interface.lan_ip_2.id
+  ]
 }
 
 resource "bizflycloud_volume" "volume1" {
@@ -54,7 +61,7 @@ resource "bizflycloud_wan_ip" "test_wan_1" {
 }
 
 data "bizflycloud_wan_ip" "wan_ip" {
-  ip_address = "103.148.57.46"
+  ip_address = "103.107.183.114"
 }
 
 data "bizflycloud_wan_ip" "wan_ip_2" {
