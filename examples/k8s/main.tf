6612b80c9a93b5b729079f5531132a880a74f8ba
duc20176723 <57260217+duc20176723@users.noreply.github.com>
2023-10-09 08:29:56 +0700

    feat: add create and update pool with taints and labels (#67)

--- examples/k8s/main.tf
+++ examples/k8s/main.tf
@@ -6,37 +6,32 @@ terraform {
   }
 }
 
+variable "EMAIL" {
+  type = string
+}
+
+variable "PASSWORD" {
+  type = string
+}
+
 provider "bizflycloud" {
   auth_method = "password"
   region_name = "HN"
-  email       = ""
-  password    = ""
+  email       = var.EMAIL
+  password    = var.PASSWORD
 }
 
 data "bizflycloud_kubernetes_version" "test_k8s_version" {
-  version = "v1.17.9"
+  version = "v1.27.4"
 }
 
 resource "bizflycloud_kubernetes" "test_k8s_cluster" {
-  name           = "tung491-test-k8s_25"
+  name           = "ducnv"
   version        = data.bizflycloud_kubernetes_version.test_k8s_version.id
-  vpc_network_id = "145bed1f-a7f7-4f88-ab3d-ce2fc95a4e71"
+  vpc_network_id = "0f05d672-6e25-439b-a27f-e7ffb44a07b2"
   tags           = ["tags", "123"]
   worker_pools {
-    name               = "pool-69643"
-    flavor             = "nix.2c_4g"
-    profile_type       = "premium"
-    volume_type        = "PREMIUM-HDD1"
-    volume_size        = 40
-    availability_zone  = "HN1"
-    desired_size       = 1
-    enable_autoscaling = true
-    min_size           = 1
-    max_size           = 3
-    tags               = ["pool_tag"]
-  }
-  worker_pools {
-    name               = "pool-9696"
+    name               = "pool-69646"
     flavor             = "nix.2c_2g"
     profile_type       = "premium"
     volume_type        = "PREMIUM-HDD1"
@@ -47,5 +42,41 @@ resource "bizflycloud_kubernetes" "test_k8s_cluster" {
     min_size           = 1
     max_size           = 3
     tags               = ["pool_tag"]
+    labels = {
+      "UpdateLabel" = "UpdateLabelVal"
+    }
+    taints {
+      effect = "NoSchedule"
+      key = "UpdateTaint"
+      value = "UpdateTaintVal"
+    }
+  }
+
+  worker_pools {
+    name               = "pool-69645"
+    flavor             = "nix.2c_2g"
+    profile_type       = "premium"
+    volume_type        = "PREMIUM-HDD1"
+    volume_size        = 40
+    availability_zone  = "HN1"
+    desired_size       = 1
+    enable_autoscaling = true
+    min_size           = 1
+    max_size           = 3
+    tags               = ["pool_tag"]
+    labels = {
+      "UpdateLabel" = "UpdateLabelVal"
+      "label2" = "labelVal2"
+    }
+    taints {
+      effect = "NoSchedule"
+      key = "UpdateTaint2"
+      value = "UpdateTaintVal2"
+    }
+    taints {
+      effect = "NoSchedule"
+      key = "UpdateTaint1"
+      value = "UpdateTaintVal1"
+    }
   }
 }
